29cc4ddc4f1131c9182b106294fac2fd49d6a5f5
Nicholas Bruning <nicholas@bruning.com.au>
2015-11-10 13:29:24 +1100

    Added tapfile

---
+++ outlaw.rb
@@ -0,0 +1,28 @@
+OUTLAW_VERSION = '0.0.1'
+OUTLAW_DIGEST = ''
+
+class Outlaw < Formula
+  homepage ""
+  url "https://github.com/hooroo/outlaw/releases/download/#{OUTLAW_VERSION}/outlaw.tar.gz"
+
+  version OUTLAW_VERSION
+  sha256  OUTLAW_DIGEST
+
+  depends_on 'docker'         => :recommended
+  depends_on 'docker-compose' => :recommended
+  depends_on 'docker-machine' => :recommended
+
+  def install
+    bin.install 'bin/outlaw'
+    prefix.install 'outlaw.yml'
+    prefix.install 'bootsync.sh'
+  end
+
+  def post_install
+    system 'outlaw', 'install'
+  end
+
+  test do
+    system 'outlaw', '-h'
+  end
+end
